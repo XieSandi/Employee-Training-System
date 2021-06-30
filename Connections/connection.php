2c02b760d5e99dcba2c7dcdf03f24bd0d21e0cab
Xie <Yue_Xie@outlook.com>
2021-06-30 12:12:38 +0700

    up

--- Connections/connection.php
+++ Connections/connection.php
@@ -7,5 +7,4 @@ $database_connection = "db_trainingsystem";
 $username_connection = "root";
 $password_connection = "";
 $connection = mysql_pconnect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
-mysql_select_db($database_connection); 
 ?>
